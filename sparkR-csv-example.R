cf1373c74a6b13a9f84372f9ea512adc01203084
pbashyal-nmdp <pbashyal@nmdp.org>
2015-07-08 11:50:27 -0500

    stop sc and upgrade to newer version.

--- sparkR-csv-example.R
+++ sparkR-csv-example.R
@@ -2,7 +2,7 @@
 library(SparkR)
 
 SPARK_HOME <- Sys.getenv("SPARK_HOME")
-SPARK_HOME <- "/Users/pbashyal/Spark/spark-1.4.1-bin-hadoop1"
+# SPARK_HOME <- "/Users/pbashyal/Spark/spark-1.4.1-bin-hadoop1"
 
 # download.file(url = "https://raw.githubusercontent.com/databricks/spark-csv/master/src/test/resources/cars.csv", 
 #               destfile = 'cars.csv',
@@ -11,7 +11,7 @@ SPARK_HOME <- "/Users/pbashyal/Spark/spark-1.4.1-bin-hadoop1"
 sc <- sparkR.init(master = "local[*]",
                   appName = "MyApplication",
                   sparkHome = SPARK_HOME,
-                  sparkPackages = "com.databricks:spark-csv_2.10:1.0.3")
+                  sparkPackages = "com.databricks:spark-csv_2.11:1.0.3")
 
 sqlContext <- sparkRSQL.init(sc)
 df <- read.df(sqlContext, "cars.csv", source = "com.databricks.spark.csv")
@@ -19,3 +19,5 @@ df <- read.df(sqlContext, "cars.csv", source = "com.databricks.spark.csv")
 #.. do stuff with df
 
 write.df(df, "newcars.csv", "com.databricks.spark.csv", "overwrite")
+
+sparkR.stop()
