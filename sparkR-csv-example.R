866d31fb48a200cd82ab7cdd8f5b565499175175
pbashyal-nmdp <pbashyal@nmdp.org>
2015-07-08 11:13:05 -0500

    changed to get cars.csv

--- sparkR-csv-example.R
+++ sparkR-csv-example.R
@@ -2,9 +2,9 @@
 library(SparkR)
 
 SPARK_HOME <- Sys.getenv("SPARK_HOME")
-SPARK_HOME <- "/Users/pbashyal/Spark/spark-1.4.0-bin-hadoop1"
+SPARK_HOME <- "/Users/pbashyal/Spark/spark-1.4.1-bin-hadoop1"
 
-# download.file(url = "https://github.com/databricks/spark-csv/raw/master/src/test/resources/cars.csv", 
+# download.file(url = "https://raw.githubusercontent.com/databricks/spark-csv/master/src/test/resources/cars.csv", 
 #               destfile = 'cars.csv',
 #               method = 'curl')  # use 'internal' on Windows
 
@@ -15,5 +15,7 @@ sc <- sparkR.init(master = "local[*]",
 
 sqlContext <- sparkRSQL.init(sc)
 df <- read.df(sqlContext, "cars.csv", source = "com.databricks.spark.csv")
+
+#.. do stuff with df
 
 write.df(df, "newcars.csv", "com.databricks.spark.csv", "overwrite")
